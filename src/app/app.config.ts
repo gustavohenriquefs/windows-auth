9a57355feb0fd4b8557a9279b8171383a8b88042
gustavohenriquefs <gustavohenriquefs.dev@gmail.com>
2023-12-23 13:33:24 -0300

    config: loginReducer

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,15 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
+import { loginReducer } from './store/login-page.reducer';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    importProvidersFrom(StoreModule.forRoot({ login: loginReducer })),
+    provideRouter(routes), 
+    provideClientHydration()
+  ]
 };
