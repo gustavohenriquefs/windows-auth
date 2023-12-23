d9f830bebb79398fae37d2bbd51313cfc09bb72a
gustavohenriquefs <gustavohenriquefs.dev@gmail.com>
2023-12-23 00:35:27 -0300

    create: user repository

---
+++ src/app/domain/repositories/user.repository.ts
@@ -0,0 +1,8 @@
+import { Observable } from "rxjs"
+import { UserModel } from "../models/user.model";
+
+export abstract class UserRepository {
+  abstract login(params: { email: string, password: string }): Observable<UserModel>;
+  abstract register(params: { name: string, email: string, password: string }): Observable<UserModel>;
+  abstract getUserProfile(): Observable<UserModel>;
+}
