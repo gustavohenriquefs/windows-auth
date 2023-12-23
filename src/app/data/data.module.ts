15039bf5c6fa58554731eb258aab1a6c32c66b8d
gustavohenriquefs <gustavohenriquefs.dev@gmail.com>
2023-12-23 13:33:46 -0300

    config: data module

---
+++ src/app/data/data.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { UserImplementationRepository } from './repositories/user/user-implementation.repository';
+import { UserRepository } from '../domain/repositories/user.repository';
+import { UserRegisterUseCase } from '../domain/usecases/user-register.usecase';
+import { GetUserProfileUseCase } from '../domain/usecases/get-user-profile.usecase';
+
+const userRegisterUseCaseFactory = (userRepo: UserRepository) => new UserRegisterUseCase(userRepo);
+export const userRegisterUseCaseProvider = {
+  provide: UserRegisterUseCase,
+  useFactory: userRegisterUseCaseFactory,
+  deps: [UserRepository],
+};
+
+const getUserProfileUseCaseFactory = (userRepo: UserRepository) => new GetUserProfileUseCase(userRepo);
+export const getUserProfileUseCaseProvider = {
+  provide: GetUserProfileUseCase,
+  useFactory: getUserProfileUseCaseFactory,
+  deps: [UserRepository],
+};
+
+@NgModule({
+  providers: [
+      userRegisterUseCaseProvider,
+      getUserProfileUseCaseProvider,
+      { provide: UserRepository, useClass: UserImplementationRepository },
+  ],
+  imports: [
+      CommonModule,
+      HttpClientModule,
+  ],
+})
+export class DataModule { }
