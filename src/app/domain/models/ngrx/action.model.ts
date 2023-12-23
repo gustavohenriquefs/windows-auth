73a6ef3a1687a191656be562e2248bedaee0113d
gustavohenriquefs <gustavohenriquefs.dev@gmail.com>
2023-12-23 00:42:13 -0300

    doc: NGRX Action interface

--- src/app/domain/models/ngrx/action.model.ts
+++ src/app/domain/models/ngrx/action.model.ts
@@ -1,3 +1,14 @@
+/**
+ * Interface for NGRX Actions: describe 
+ * the action that will be dispatched in 
+ * you application
+ * 
+ * @example
+ * content_copy
+ *  {
+ *    type: '[Auth API] Login Success'
+ *  }
+ */
 interface Action {
   type: string;
 }
