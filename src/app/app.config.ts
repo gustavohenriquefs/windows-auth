6b2cf0f6bea6fc300ccb63283335d72e1a812c70
gustavo henrique <gustavohenriquefreitasdesousa@gmail.com>
2023-12-25 17:51:24 -0300

    add: images, styling: reponsive design

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,25 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { StoreModule } from '@ngrx/store';
-import { loginReducer } from './store/login-page.reducer';
+import { StoreModule, provideState, provideStore } from '@ngrx/store';
+import { loginReducer } from './store/login-page/login-page.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { LoginPageStore } from './store/login-page/login-page.effects';
+import { UserImplementationRepository } from './data/repositories/user/user-implementation.repository';
+import { HttpClientModule } from '@angular/common/http';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(StoreModule.forRoot({ login: loginReducer })),
+    UserImplementationRepository,
+    provideStore(),
+    provideState({ name: 'login', reducer: loginReducer }),
+    importProvidersFrom(
+      StoreModule.forRoot({ login: loginReducer }),
+      EffectsModule.forRoot([LoginPageStore]),    
+      HttpClientModule,
+      FontAwesomeModule
+    ),
     provideRouter(routes), 
     provideClientHydration()
   ]
