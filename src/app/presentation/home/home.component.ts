ca358a24b0ce8a806212373fa80892724de55193
gustavohenriquefs <gustavohenriquefs.dev@gmail.com>
2024-02-28 13:21:59 -0300

    feat: implement home

--- src/app/presentation/home/home.component.ts
+++ src/app/presentation/home/home.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import { faFileAlt, faListAlt } from '@fortawesome/free-regular-svg-icons';
+
+interface TabItem {
+  name: string;
+  icon: IconDefinition;
+}
+
+interface DataHomeTab {
+  selectedTab: string;
+  tabItems: TabItem[];
+}
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    FontAwesomeModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  dataHomeTabs: DataHomeTab = {
+    selectedTab: 'View',
+    tabItems: [
+      {
+        name: 'View',
+        icon: faListAlt
+      },
+      {
+        name: 'Edit',
+        icon: faFileAlt
+      }
+    ]};
 
+  selectTab(tabName: string): void {
+    this.dataHomeTabs.selectedTab = tabName;
+  }
 }
